94fdef31c822b731d2d7835eb81c2b75c34f7e16
black <cool1726@naver.com>
2020-07-31 10:30:25 +0900

    no venv

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\siwon\2020\likelion\0604 djangocrud\crreview\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
